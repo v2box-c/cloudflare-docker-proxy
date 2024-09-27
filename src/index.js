be0214e57f6230e69008912fa33d7547388f5c52
v2box-c <1@rnmtq.eu>
2024-09-27 15:04:30 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.980303.xyz": dockerHub,
+  "quay.980303.xyz": "https://quay.io",
+  "gcr.980303.xyz": "https://gcr.io",
+  "k8s-gcr.980303.xyz": "https://k8s.gcr.io",
+  "k8s.980303.xyz": "https://registry.k8s.io",
+  "ghcr.980303.xyz": "https://ghcr.io",
+  "cloudsmith.980303.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.980303.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
